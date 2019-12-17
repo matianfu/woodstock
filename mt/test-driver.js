230116d820db71dd99b9b543f48d2e029a268014
matianfu <matianfu@gmail.com>
2019-12-17 14:42:03 +0800

    clean old files

--- mt/test-driver.js
+++
@@ -1,7 +0,0 @@
-const DBusDriver = require('./src/dbus-driver')
-
-const driver = new DBusDriver()
-driver.on('connect', () => console.log('connect'))
-driver.on('invocation', m => console.log('invoke', m))
-
-console.log(driver)

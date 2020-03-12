b5ae870dd50512ee1452b4c51df2df6300e615df
matianfu <matianfu@gmail.com>
2020-03-12 21:06:50 +0800

    udpate ble demo to use new addNode

--- demos/ble.js
+++ demos/ble.js
@@ -3,151 +3,108 @@ const { BYTE, BOOLEAN, STRING, OBJECT_PATH, ARRAY, VARIANT } = woodstock.types
 
 const dbus = woodstock()
 
-dbus.addNode({
-  path: '/',
-  implementations: ['org.freedesktop.DBus.Properties']
+dbus.addNode('/', 'org.freedesktop.DBus.Properties')
+
+dbus.addNode('/adv', 'org.freedesktop.DBus.Properties', {
+  interface: 'org.bluez.LEAdvertisement1',
+  Type: new STRING('peripheral'),
+  LocalName: new STRING('Hello'),
+  ServiceUUIDs: new ARRAY('as'),
+  Release () { }
 })
 
-dbus.addNode({
-  path: '/adv',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    {
-      interface: 'org.bluez.LEAdvertisement1',
-      Type: new STRING('peripheral'),
-      LocalName: new STRING('Hello'),
-      ServiceUUIDs: new ARRAY('as'),
-      Release () { }
-    }
-  ]
-})
+dbus.addNode('/com/example/gatt',
+  'org.freedesktop.DBus.Properties',
+  'org.freedesktop.DBus.ObjectManager')
 
-dbus.addNode({
-  path: '/com/example/gatt',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    'org.freedesktop.DBus.ObjectManager'
-  ]
-})
+dbus.addNode('/com/example/gatt/service001',
+  'org.freedesktop.DBus.Properties', {
+    interface: 'org.bluez.GattService1',
+    UUID: new STRING('60000000-0182-406c-9221-0a6680bd0943'),
+    Primary: new BOOLEAN(true)
 
-dbus.addNode({
-  path: '/com/example/gatt/service001',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    {
-      interface: 'org.bluez.GattService1',
-      UUID: new STRING('60000000-0182-406c-9221-0a6680bd0943'),
-      Primary: new BOOLEAN(true)
+    // These two properties are harmless and useless as of this writing
+    //
+    // Device: new OBJECT_PATH('/org/bluez/hci0'),
+    // Characteristics: new ARRAY([
+    //   new OBJECT_PATH('/com/example/gatt/service001/char001')
+    // ])
+  })
 
-      // These two properties are harmless and useless as of this writing
-      //
-      // Device: new OBJECT_PATH('/org/bluez/hci0'),
-      // Characteristics: new ARRAY([
-      //   new OBJECT_PATH('/com/example/gatt/service001/char001')
-      // ])
-    }
-  ]
-})
+dbus.addNode('/com/example/gatt/service001/char001',
+  'org.freedesktop.DBus.Properties', {
+    interface: 'org.bluez.GattCharacteristic1',
+    UUID: new STRING('60000002-0182-406c-9221-0a6680bd0943'),
+    Service: new OBJECT_PATH('/com/example/gatt/service001'),
+    Value: new ARRAY('ay', [new BYTE('f'), new BYTE('o'), new BYTE('o')]),
+    Flags: new ARRAY('as', [new STRING('read'), new STRING('notify')]),
+    ReadValue (m) {
+      console.log('read value', m, this.Value)
+      return this.Value
+    },
+    WriteValue (m) {
+      // NotSupported or NotPermitted ???
+    },
+    StartNotify () { },
+    StopNotify () { }
+  })
 
-dbus.addNode({
-  path: '/com/example/gatt/service001/char001',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    {
-      interface: 'org.bluez.GattCharacteristic1',
-      UUID: new STRING('60000002-0182-406c-9221-0a6680bd0943'),
-      Service: new OBJECT_PATH('/com/example/gatt/service001'),
-      Value: new ARRAY('ay', [new BYTE('f'), new BYTE('o'), new BYTE('o')]),
-      Flags: new ARRAY('as', [new STRING('read'), new STRING('notify')]),
-      ReadValue (m) {
-        console.log('read value', m, this.Value)
-        return this.Value
-      },
-      WriteValue (m) {
-        // NotSupported or NotPermitted ???
-      },
-      StartNotify () { },
-      StopNotify () { }
-    }
-  ]
-})
+dbus.addNode('/com/example/gatt/service001/char002',
+  'org.freedesktop.DBus.Properties', {
+    interface: 'org.bluez.GattCharacteristic1',
+    UUID: new STRING('60000003-0182-406c-9221-0a6680bd0943'),
+    Service: new OBJECT_PATH('/com/example/gatt/service001'),
+    Value: new ARRAY('ay', [new BYTE('b'), new BYTE('a'), new BYTE('r')]),
+    Flags: new ARRAY('as', [new STRING('read'), new STRING('write')]),
+    ReadValue (m) { return this.Value },
+    WriteValue (m) {
+      this.node.invoke({
+        interface: 'org.freedesktop.DBus.Properties',
+        member: 'Set',
+        body: [
+          new STRING('org.bluez.GattCharacteristic1'),
+          new STRING('Value'),
+          new VARIANT(m.body[0])
+        ],
+        sender: m.sender
+      })
+    },
+    StartNotify () { },
+    StopNotify () { }
+  })
 
-dbus.addNode({
-  path: '/com/example/gatt/service001/char002',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    {
-      interface: 'org.bluez.GattCharacteristic1',
-      UUID: new STRING('60000003-0182-406c-9221-0a6680bd0943'),
-      Service: new OBJECT_PATH('/com/example/gatt/service001'),
-      Value: new ARRAY('ay', [new BYTE('b'), new BYTE('a'), new BYTE('r')]),
-      Flags: new ARRAY('as', [new STRING('read'), new STRING('write')]),
-      ReadValue (m) { return this.Value },
-      WriteValue (m) {
-        this.node.invoke({
-          interface: 'org.freedesktop.DBus.Properties',
-          member: 'Set',
-          body: [
-            new STRING('org.bluez.GattCharacteristic1'),
-            new STRING('Value'),
-            new VARIANT(m.body[0])
-          ],
-          sender: m.sender
-        })
-      },
-      StartNotify () { },
-      StopNotify () { }
-    }
-  ]
-})
+dbus.addNode('/com/example/gatt/service002',
+  'org.freedesktop.DBus.Properties', {
+    interface: 'org.bluez.GattService1',
+    UUID: new STRING('70000000-0182-406c-9221-0a6680bd0943'),
+    Primary: new BOOLEAN(true)
+  })
 
-dbus.addNode({
-  path: '/com/example/gatt/service002',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    {
-      interface: 'org.bluez.GattService1',
-      UUID: new STRING('70000000-0182-406c-9221-0a6680bd0943'),
-      Primary: new BOOLEAN(true)
-    }
-  ]
-})
+dbus.addNode('/com/example/gatt/service002/char001',
+  'org.freedesktop.DBus.Properties', {
+    interface: 'org.bluez.GattCharacteristic1',
+    UUID: new STRING('70000002-0182-406c-9221-0a6680bd0943'),
+    Service: new OBJECT_PATH('/com/example/gatt/service002'),
+    Value: new ARRAY('ay', [new BYTE('f'), new BYTE('o'), new BYTE('o')]),
+    Flags: new ARRAY('as', [new STRING('read'), new STRING('notify')]),
+    ReadValue (m) { return this.Value },
+    WriteValue (m) { console.log(m) },
+    StartNotify () { },
+    StopNotify () { }
+  })
 
-dbus.addNode({
-  path: '/com/example/gatt/service002/char001',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    {
-      interface: 'org.bluez.GattCharacteristic1',
-      UUID: new STRING('70000002-0182-406c-9221-0a6680bd0943'),
-      Service: new OBJECT_PATH('/com/example/gatt/service002'),
-      Value: new ARRAY('ay', [new BYTE('f'), new BYTE('o'), new BYTE('o')]),
-      Flags: new ARRAY('as', [new STRING('read'), new STRING('notify')]),
-      ReadValue (m) { return this.Value },
-      WriteValue (m) { console.log(m) },
-      StartNotify () { },
-      StopNotify () { }
-    }
-  ]
-})
-
-dbus.addNode({
-  path: '/com/example/gatt/service002/char002',
-  implementations: [
-    'org.freedesktop.DBus.Properties',
-    {
-      interface: 'org.bluez.GattCharacteristic1',
-      UUID: new STRING('70000003-0182-406c-9221-0a6680bd0943'),
-      Service: new OBJECT_PATH('/com/example/gatt/service002'),
-      Value: new ARRAY('ay', [new BYTE('b'), new BYTE('a'), new BYTE('r')]),
-      Flags: new ARRAY('as', [new STRING('read'), new STRING('write')]),
-      ReadValue (m) { },
-      WriteValue (m) { },
-      StartNotify () { },
-      StopNotify () { }
-    }
-  ]
-})
+dbus.addNode('/com/example/gatt/service002/char002',
+  'org.freedesktop.DBus.Properties', {
+    interface: 'org.bluez.GattCharacteristic1',
+    UUID: new STRING('70000003-0182-406c-9221-0a6680bd0943'),
+    Service: new OBJECT_PATH('/com/example/gatt/service002'),
+    Value: new ARRAY('ay', [new BYTE('b'), new BYTE('a'), new BYTE('r')]),
+    Flags: new ARRAY('as', [new STRING('read'), new STRING('write')]),
+    ReadValue (m) { },
+    WriteValue (m) { },
+    StartNotify () { },
+    StopNotify () { }
+  })
 
 dbus.on('connect', () => {
   dbus.AddMatch({
